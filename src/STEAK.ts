0a49429acecd237ec51c9b35e18596fbff80e924
deggen <d.kellenschwiler@bsvblockchain.org>
2024-05-02 13:06:16 -0500

    Update STEAK.ts

--- src/STEAK.ts
+++ src/STEAK.ts
@@ -4,7 +4,7 @@ import { AdmittanceInstructions } from "./AdmittanceInstructions.js"
  * Submitted Transaction Execution AcKnowledgment
  *
  * @description
- * Comprises the topics where a transaction was submitted, and for each one, the output indecies for the UTXOs newly admitted into the topics, and the coins retained.
+ * Comprises the topics where a transaction was submitted, and for each one, the output indices for the UTXOs newly admitted into the topics, and the coins retained.
  * An object whose keys are topic names and whose values are topical admittance instructions denoting the state of the submitted transaction with respect to the associated topic.
  */
 export type STEAK = Record<string, AdmittanceInstructions>

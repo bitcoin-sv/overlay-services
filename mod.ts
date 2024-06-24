34fee1bbec2840b21fbc047109e50aada4eeafbf
David Case <david.case@shruggr.cloud>
2024-06-24 16:26:54 +0000

    fix import

--- mod.ts
+++ mod.ts
@@ -4,7 +4,7 @@ export type { LookupService } from "./src/LookupService.js"
 export type { TopicManager } from "./src/TopicManager.js"
 
 // Interfaces and structures
-export type { Storage } from "./src/storage/Storage.js"
+export type { Storage, AppliedTransaction } from "./src/storage/Storage.js"
 export type { Output } from './src/Output.js'
 export type { AdmittanceInstructions } from './src/AdmittanceInstructions.js'
 export type { TaggedBEEF } from './src/TaggedBEEF.js'
@@ -14,6 +14,5 @@ export type { LookupFormula } from './src/LookupFormula.js'
 export type { LookupAnswer } from './src/LookupAnswer.js'
 
 // The Knex storage system
-export * from './src/storage/Storage.js'
 export { KnexStorage } from './src/storage/knex/KnexStorage.js'
 export * as KnexStorageMigrations from './src/storage/knex/all-migrations.js'

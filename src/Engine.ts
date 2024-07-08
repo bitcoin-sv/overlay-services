06fd9a4b60f798186bd22a6bdbba46db822982be
Ty Everett <ty@tyweb.us>
2024-07-08 13:03:17 -0700

    Update src/Engine.ts

--- src/Engine.ts
+++ src/Engine.ts
@@ -1019,6 +1019,7 @@ export class OverlayGASPStorage implements GASPStorage {
       // The transaction is not admissible, get inputs needed for further verification
       // TopicManagers should implement a function to identify which inputs are needed.
       if (this.engine.managers[this.topic] !== undefined && typeof this.engine.managers[this.topic].identifyNeededInputs === 'function') {
+        // In case the topic manager isn't able to stipulate needed inputs, we need to request all inputs as if we had no merkle proof. However, it's dubious that we sometimes don't know — QUESTION: Should we require all topic managers to support this functionality? The alternative to requiring ALL inputs by default is to require NO inputs by default, cutting off the historical graph at this point (e.g. `return undefined`).
         for (const input of parsedTx.inputs) {
           response.requestedInputs[`${input.sourceTXID}.${input.sourceOutputIndex}`] = {
             metadata: false

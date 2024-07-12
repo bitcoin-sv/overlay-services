4a1a3fd39488e6c77ccfbbc694cc6b74d3503fbe
Brayden Langley <brayden167@gmail.com>
2024-07-12 16:29:35 -0700

    Block zero indicates select all

--- src/storage/knex/all-migrations.ts
+++ src/storage/knex/all-migrations.ts
@@ -1,14 +1,17 @@
 import { Knex } from 'knex'
-import { up as oneUp, down as oneDown } from './migrations/2024-05-18-001-initial.js'
+import { up as initialSchemaUp, down as initialSchemaDown } from './migrations/2024-05-18-001-initial.js'
+import { up as addBlockHeightColumnUp, down as addBlockHeightColumnDown } from './migrations/2024-07-10-001-block-height.js'
 
 /**
  * An array of all migrations, in order.
  */
-const allMigrations: {
-    up: (knex: Knex) => Promise<void>
-    down: (knex: Knex) => Promise<void>
-}[] = [
-        { up: oneUp, down: oneDown }
-    ]
+type Migration = {
+  up: (knex: Knex) => Promise<void>;
+  down: (knex: Knex) => Promise<void>;
+};
 
+const allMigrations: Migration[] = [
+  { up: initialSchemaUp, down: initialSchemaDown },
+  { up: addBlockHeightColumnUp, down: addBlockHeightColumnDown }
+];
 export default allMigrations
